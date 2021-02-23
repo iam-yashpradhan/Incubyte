03514ca3c1e4ececca2956280dc6b3aca57a7919
Yash Pradhan <mrpradhan555@gmail.com>
2021-02-23 17:59:33 +0530

    support for different delimiters

--- src/pack/incubyte.java
+++ src/pack/incubyte.java
@@ -2,10 +2,19 @@ package pack;
 
 public class incubyte {
     public static int Add(String numbers){
-        int sum=0;
+        int sum=0;String temp[];
         if(numbers.equals(""))
             return  0;
-        String temp[]=numbers.split(",|\n");
+        if(numbers.length()>2 && numbers.substring(0,2).equals("//")){
+            int newLinePosition=numbers.indexOf('\n');
+            String delim=numbers.substring(2,newLinePosition);
+            numbers=numbers.substring(newLinePosition+1);
+            temp=numbers.split(delim);
+        }else if(numbers.contains(",")||numbers.contains("\n")){
+            temp=numbers.split(",|\n");
+        }else{
+            return Integer.parseInt(numbers);
+        }
         for(int i=0;i< temp.length;i++){
             sum+=Integer.parseInt(temp[i]);
         }
@@ -13,7 +22,7 @@ public class incubyte {
     }
 
     public static void main(String args[]){
-        String test="1,2\n3";
+        String test="//;\n2;4";
         System.out.println(Add(test));
     }
 }
