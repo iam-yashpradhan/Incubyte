00bee601c03d6fbf3e8241dc929b21809988f196
Yash Pradhan <mrpradhan555@gmail.com>
2021-02-23 18:01:43 +0530

    Support for multiple delimiters of any length

--- src/pack/incubyte.java
+++ src/pack/incubyte.java
@@ -5,13 +5,32 @@ import java.util.ArrayList;
 public class incubyte {
     public static int Add(String numbers) throws Exception {
         int sum=0;String temp[];
+        ArrayList<String> delimiters=new ArrayList<>();
         if(numbers.equals(""))
             return  0;
         if(numbers.length()>2 && numbers.substring(0,2).equals("//")){
             int newLinePosition=numbers.indexOf('\n');
-            String delim=numbers.substring(2,newLinePosition);
+            String delims=numbers.substring(2,newLinePosition);
+            for(int i=0;i<delims.length();i++){
+                if(delims.charAt(i)=='['){
+                    int j=i;
+                    while(delims.charAt(i)!=']')
+                        i++;
+                    String delim=delims.substring(j+1,i);
+                    delimiters.add(delim);
+                }
+            }
             numbers=numbers.substring(newLinePosition+1);
-            temp=numbers.split(delim);
+            String delimsList="";
+            for(int i=0;i< delimiters.size();i++){
+                String delim=delimiters.get(i);
+                if(i+1==delimiters.size()){
+                    delimsList+=delim;
+                    break;
+                }
+                delimsList+=delim+"|";
+            }
+            temp=numbers.split(delimsList);
         }else if(numbers.contains(",")||numbers.contains("\n")){
             temp=numbers.split(",|\n");
         }else{
@@ -26,6 +45,8 @@ public class incubyte {
                     neg=true;
                     negs.add(a);
                 }else {
+                    if(a>1000)
+                        a=0;
                     sum += a;
                 }
             }
@@ -37,7 +58,7 @@ public class incubyte {
     }
 
     public static void main(String args[]) throws Exception {
-        String test="//;\n2;-4;-4";
+        String test="//[%][,;]\n4%5,;4";
         System.out.println(Add(test));
     }
 }
